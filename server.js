5400d9cc2872f4a228254f27e5408ab91908452a
Jesse Shaw <jdshaw1987@gmail.com>
2017-09-14 19:28:26 -0400

    Starting to build API

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080ç;
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+
+console.log('todo list RESTful API server started on: ' + port);
 
 exports.app = app;
