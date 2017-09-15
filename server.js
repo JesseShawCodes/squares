1df50f7a5f8afcef11b99260c529f8d51f6412c2
Jesse Shaw <jdshaw1987@gmail.com>
2017-09-15 09:06:22 -0400

    Updated server.js file

--- server.js
+++ server.js
@@ -3,7 +3,32 @@ var app = express();
 var port = process.env.PORT || 8080ç;
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+mongoose = require('mongoose'),
+Task = require('./api/models/todoListModel'), //created model loading here
+bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
 
 console.log('todo list RESTful API server started on: ' + port);
 
 exports.app = app;
+
+
+
+
+
+
+
