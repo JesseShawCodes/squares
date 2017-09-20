783709753dd4320640de2dabbe79d5b3740256cb
Jesse Shaw <jdshaw1987@gmail.com>
2017-09-19 21:31:02 -0400

    New Commit

--- public/index.js
+++ public/index.js
@@ -1,26 +1,13 @@
 $("form").on("submit", function(e) {
     e.preventDefault();
-    console.log("Test");
     var title = $(".title").val();
     var description = $(".description").val();
     var link = $(".link").val();
-<<<<<<< HEAD
-    var category = $(".category").val();
     var dataInput = {
         name: title,
         description: description,
-        link: link,
-        category: category
-    };
-=======
-    var data = [title, description, link];
-    $.post('api', {
-        title: title,
-        description: description,
         link: link
-    });
-    console.log(data);
->>>>>>> parent of 67901b0... Updating Post Request functionality
+    };
     $("#grid").append(`
     <section class="resource">
         <span><h1>${title}</h1></span> 
@@ -28,4 +15,12 @@ $("form").on("submit", function(e) {
         <span>${link}</span>
     </section>
     `);
-})
+    console.log(dataInput.name);
+    $.ajax({
+        type: "POST",
+        url: "/api",
+        data: dataInput,
+        success: null,
+        dataType: 'json'
+    });
+});
