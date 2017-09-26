0703541e9a38ebd48aaa5b038c1d2225930e1a40
Jesse Shaw <jdshaw1987@gmail.com>
2017-09-25 22:58:52 -0400

    Updating to include Mongo

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                             global.DATABASE_URL ||
-                            'mongodb://thejesseshaw:Mbdtf87!@ds139964.mlab.com:39964/capstone2' ||
-                            'mongodb://localhost:8080/api'
+                            'mongodb://thejesseshaw:Mbdtf87!@ds139964.mlab.com:39964/capstone2'
 exports.PORT = process.env.PORT || 8080;
